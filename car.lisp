9f4054de9b4ddad46935ef1e12f083e0b72d53c0
treep <zena.treep@gmail.com>
2010-06-18 20:18:51 +0400

    Added S-FORM representation for CARs. Added some empty files.

--- car.lisp
+++ car.lisp
@@ -1,15 +1,14 @@
 ;;;
 ;;; car.lisp --- Technical Specification of Cars.
+;;;              or
+;;;              CLOS/S-CAR-FORMs representations of CARs.
 ;;;
 
-;;; CAR classes:
-
-;; FIXME: may be we need to drop out this class `fuel-tank'
-(defclass fuel-tank ()
-  ((name :reader name :initarg :name :type symbol)))
+;;; CLOS representation:
 
 (defclass section ()
-  ((fuel-tank :reader fuel-tank :initarg :fuel-tank :type fuel-tank)))
+  ((fuel-tank  :reader   fuel-tank  :initarg  :fuel-tank :type symbol)
+   (fuel-value :accessor fuel-value :initform 0          :type integer)))
 
 (defclass pipe ()
   ((sections :reader sections :initarg :sections :type list)))
@@ -21,35 +20,47 @@
 (defclass car-engine ()
   ((chambers :reader chambers :initarg :chambers :type list)))
 
-;;; CAR constructors:
+;;; Representation in S-CAR-FORMs:
+;;;   Each CAR parametrizated with list of two lists of fuel-tank names.
 
-(defmethod make-fuel-tank (name)
-  (make-instance 'fuel-tank
-                 :name name))
+;;; S-CAR-FORM -> CLOS convertion:
 
-(defmethod make-section (name)
+(defun make-section (name)
   (make-instance 'section
-                 :fuel-tank (make-fuel-tank name)))
+                 :fuel-tank name))
 
-(defmethod make-pipe (names)
+(defun make-pipe (names)
   (make-instance 'pipe
                  :sections (mapcar #'make-section names)))
 
-(defmethod make-chamber (upper-names/lower-names)
+(defun make-chamber (upper-names/lower-names)
   (make-instance 'chamber
                  :upper-pipe (make-pipe (first  upper-names/lower-names))
                  :lower-pipe (make-pipe (second upper-names/lower-names))))
 
-(defmethod make-car-engine (chambers-names)
+(defun make-car-engine (s-car-form)
   (make-instance 'car-engine
-                 :chambers (mapcar #'make-chamber chambers-names)))
+                 :chambers (mapcar #'make-chamber s-car-form)))
 
-;;; PRINT-OBJECT for CAR:
+;;; CLOS -> S-CAR-FORM with keys convertion:
 
-;; FIXME: more pretty-printing.
+(defmethod s-section ((section section))
+  (list :section (fuel-tank section) (fuel-value section)))
+
+(defmethod s-pipe ((pipe pipe))
+  (cons :pipe (mapcar #'s-section (sections pipe))))
+
+(defmethod s-chamber ((chamber chamber))
+  (list (cons :upper-pipe (s-pipe (upper-pipe chamber)))
+        (cons :lower-pipe (s-pipe (lower-pipe chamber)))))
+
+(defmethod s-car-engine ((car-engine car-engine))
+  (cons :car-engine (mapcar #'s-chamber (chambers car-engine))))
+
+;;; PRINT-OBJECT for CAR objects:
 
 (defmethod print-object ((section section) stream)
-  (format stream "<:section to fuel-tank ~A>" (name (fuel-tank section))))
+  (format stream "<:section to fuel-tank ~A>" (fuel-tank section)))
 
 (defmethod print-object ((pipe pipe) stream)
   (mapcar #'(lambda (e) (print e stream))
@@ -62,18 +73,6 @@
 (defmethod print-object ((car-engine car-engine) stream)
   (mapcar #'(lambda (e) (print e stream))
           (cons :car-engine (chambers car-engine))))
-
-;;; CAR DSL:
-
-;; TODO: написать представление машин в виде s-форм с ключами.
-
-;; (defmacro define-car 
-
-;;; Theorem
-
-#|
-  Each CAR parametrizated with list of two lists of fuel-tank names.
-|#
 
 ;;; Example
 
@@ -96,13 +95,30 @@
 
 ;; =>
 
-;; :CAR-ENGINE
-;; :CHAMBER
-;; :PIPE
-;; <:section to fuel-tank FUEL0>
-;; <:section to fuel-tank FUEL0>
-;;
-;; :PIPE
-;; <:section to fuel-tank FUEL0>
-;; <:section to fuel-tank FUEL1>
-;; <:section to fuel-tank FUEL0>
+#|
+:CAR-ENGINE 
+
+:CHAMBER 
+
+:PIPE 
+<:section to fuel-tank FUEL0> 
+<:section to fuel-tank FUEL0>  
+
+:PIPE 
+<:section to fuel-tank FUEL0> 
+<:section to fuel-tank FUEL1> 
+<:section to fuel-tank FUEL0>
+|#
+
+(s-car-engine
+ (make-car-engine
+  '(((fuel0 fuel0)
+     (fuel0 fuel1 fuel0)))))
+
+;; =>
+
+#|
+(:CAR-ENGINE
+ ((:UPPER-PIPE :PIPE (:SECTION FUEL0 0) (:SECTION FUEL0 0))
+  (:LOWER-PIPE :PIPE (:SECTION FUEL0 0) (:SECTION FUEL1 0) (:SECTION FUEL0 0))))
+|#
