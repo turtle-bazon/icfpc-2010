043c9714d362f229a4e753bb64fab8aa95a36147
vseloved <vseloved@gmail.com>
2010-06-18 22:30:39 +0300

    A little clean-up of gen-fuel

--- gen-fuel.lisp
+++ gen-fuel.lisp
@@ -1,3 +1,5 @@
+(in-package :cl-user)
+
 (defun strings (&rest strings)
   (apply #'concatenate 'string strings))
 
@@ -5,8 +7,8 @@
   (let ((list nil))
     (dotimes (i (1- nodes-num))
       (let ((strnum (write-to-string i)))
-	(push (strings strnum "L") list)
-	(push (strings strnum "R") list)))
+        (push (strings strnum "L") list)
+        (push (strings strnum "R") list)))
     (push "X" list)
     (push (strings (write-to-string (1- nodes-num)) "R") list)
     (nreverse list)))
@@ -20,22 +22,24 @@
 
 (defun random-column (nodes-num)
   (let ((strlst nil)
-	(connects (shuffle-list (possible-connects-list nodes-num))))
+        (connects (shuffle-list (possible-connects-list nodes-num))))
     (dotimes (i nodes-num)
       (push (strings (pop connects) (pop connects)) strlst))
     (nreverse strlst)))
 
 (defun random-factory (nodes-num)
   (let ((left-column (random-column nodes-num))
-	(right-column (random-column nodes-num))
-	(header (strings (write-to-string (1- nodes-num)) "L"))
-	(factory nil))
+        (right-column (random-column nodes-num))
+        (header (strings (write-to-string (1- nodes-num)) "L"))
+        (factory nil))
     (push (strings header ":") factory)
     (dotimes (i (1- nodes-num))
-      (push (strings (elt left-column i) "0#" (elt right-column i) ",") factory))
-    (push (strings (elt left-column (1- nodes-num)) "0#"
-		   (elt right-column (1- nodes-num)) ":")
-	  factory)
+      (push (strings (elt left-column  i) "0#"
+                     (elt right-column i) ",")
+            factory))
+    (push (strings (elt left-column  (1- nodes-num)) "0#"
+                   (elt right-column (1- nodes-num)) ":")
+          factory)
     (push header factory)
     (apply #'strings (nreverse factory))))
 
@@ -43,25 +47,24 @@
   (dotimes (i (length factory))
     (format stream "~c" (elt factory i))
     (when (or (char= (elt factory i) #\,)
-	      (char= (elt factory i) #\:))
+              (char= (elt factory i) #\:))
       (terpri stream))))
 
 (defun submit-factory (factory)
   (apply #'strings
-	 (coerce (with-auth (post-fuel "219" factory)) 'list)))
+         (coerce (with-auth (post-fuel "219" factory)) 'list)))
 
 (defun test-submit-factory (factory)
-  (let ((string
-	 (submit-factory factory)))
+  (let ((string (submit-factory factory)))
     (when (not (or (ppcre:scan-to-strings "unexpected" string)
-		   (ppcre:scan-to-strings "inconsistent" string)
-		   (ppcre:scan-to-strings "illegal" string)))
+                   (ppcre:scan-to-strings "inconsistent" string)
+                   (ppcre:scan-to-strings "illegal" string)))
       (list factory string))))
 
 (defun search-proper-factories (factory-size max-iter)
   (let ((proper-factories nil))
     (dotimes (i max-iter)
       (let ((result (test-submit-factory (random-factory factory-size))))
-	(if result (push result proper-factories))))
+        (if result (push result proper-factories))))
     (nreverse proper-factories)))
       
