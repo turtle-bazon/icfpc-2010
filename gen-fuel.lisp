7abb398c14d902beab337930048a7a4e13b7bf4c
vseloved <vseloved@gmail.com>
2010-06-19 12:07:08 +0300

    Added in/out to make-node-array

--- gen-fuel.lisp
+++ gen-fuel.lisp
@@ -108,14 +108,14 @@
            (setf 1st (remove r1 1st)
                  2nd (remove r2 2nd))))
        (when (connected? rez)
-         (retrun rez)))))
+         (return rez)))))
 
 (defun connected? (2d-array)
   (let* ((n (array-dimension 2d-array 0))
          (gates (range n)))
     (labels ((recur (next step)
                (unless (= step 0)
-                 (print (setf gates (remove next gates)))
+                 (setf gates (remove next gates))
                  (unless gates
                    (return-from connected? t))
                  (recur (aref 2d-array next 0) (1- step))
@@ -123,6 +123,38 @@
       (recur 0 n))))
 
 (defun make-circuit (n)
-  (let ((proto (make-node-array n)))
-    (dolist (i n)
-      (
+  (let ((proto (make-node-array n))
+        rez)
+    (dotimes (i n)
+      (dotimes (j 4)
+        (let* ((circuit (copy-array proto))
+               (j1 (if (logbitp 0 j) 1 0))
+               (j2 (if (logbitp 1 j) 2 3))
+               (in-subst (aref circuit i j1))
+               (ou-subst (aref circuit i j2))
+               (in-j (if (= (aref circuit in-subst 2) i) 2 3))
+               (ou-j (if (= (aref circuit ou-subst 0) i) 0 1)))
+            (setf (aref circuit in-subst in-j) ou-subst
+                  (aref circuit ou-subst ou-j) in-subst
+                  (aref circuit i j1) "X"
+                  (aref circuit i j2) "X")
+            (push circuit rez))))
+    (values (random-elt rez)
+            rez)))
+
+(defun copy-array (array &key
+                   (element-type (array-element-type array))
+                   (fill-pointer (and (array-has-fill-pointer-p array)
+                                      (fill-pointer array)))
+                   (adjustable (adjustable-array-p array)))
+  "Returns an undisplaced copy of <_:arg array />, with same
+<_:arg fill-pointer /> and <_:arg adjustab />ility (if any)
+as the original, unless overridden by the keyword arguments"
+  (let ((dims (array-dimensions array)))
+    ;; Dictionary entry for ADJUST-ARRAY requires adjusting a
+    ;; displaced array to a non-displaced one to make a copy
+    (adjust-array
+     (make-array dims
+                 :element-type element-type :fill-pointer fill-pointer
+                 :adjustable adjustable :displaced-to array)
+     dims)))
