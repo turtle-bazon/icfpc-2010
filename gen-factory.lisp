5cbc55185aeacdd9bef23c22b74e766518274a95
treep <zena.treep@gmail.com>
2010-06-20 00:59:36 +0400

    Change all files for utils.lisp.

--- gen-factory.lisp
+++ gen-factory.lisp
@@ -1,5 +1,8 @@
-(in-package :cl-user)
+;;;
+;;; random-factory.lisp
+;;;
 
+(in-package :icfpc)
 
 (defun make-gate-array (n)
   "Return 2d array of indexes, that can be used as a basis for factory"
@@ -61,31 +64,3 @@
             (push circuit rez))))
     (values (random-elt rez)
             rez)))
-
-;;; utils
-
-(defun range (n)
-  (let (rez)
-    (dotimes (i n)
-      (push i rez))
-    rez))
-
-(defun random-elt (lst)
-  (elt lst (random (length lst))))
-
-(defun copy-array (array &key
-                   (element-type (array-element-type array))
-                   (fill-pointer (and (array-has-fill-pointer-p array)
-                                      (fill-pointer array)))
-                   (adjustable (adjustable-array-p array)))
-  "Returns an undisplaced copy of <_:arg array />, with same
-<_:arg fill-pointer /> and <_:arg adjustab />ility (if any)
-as the original, unless overridden by the keyword arguments"
-  (let ((dims (array-dimensions array)))
-    ;; Dictionary entry for ADJUST-ARRAY requires adjusting a
-    ;; displaced array to a non-displaced one to make a copy
-    (adjust-array
-     (make-array dims
-                 :element-type element-type :fill-pointer fill-pointer
-                 :adjustable adjustable :displaced-to array)
-     dims)))
