a06acfab2e7aa8085cdc77b459c0a8ed5ecd3d5f
vseloved <vseloved@gmail.com>
2010-06-19 11:48:58 +0300

    Added connectedness check to make-node-array

--- gen-fuel.lisp
+++ gen-fuel.lisp
@@ -83,32 +83,46 @@
 
 (defun make-node-array (n)
   "Return 2d array of indexes, that can be used as a basis for factory"
-  (let ((rez (make-array (list n 4) :initial-element nil))
-        (1st (range n))
-        (2nd (range n))
-        (i 0))
-    (dotimes (i n)
-      (let* ((r1 (random-elt 1st))
-             (r2 (random-elt 2nd))
-             (j1 (+ 2 (random 2)))
-             (j2 (+ 2 (random 2))))
-        (when (aref rez r1 j1)
-          (setf j1 (if (= j1 2) 3 2)))
-        (when (aref rez r2 j2)
-          (setf j2 (if (= j2 2) 3 2)))
-        (when (= r1 r2)
-          (setf j1 2
-                j2 3))
-        (setf (aref rez i 0)   r1
-              (aref rez i 1)   r2
-              (aref rez r1 j1) i
-              (aref rez r2 j2) i)
+  (loop
+     (let ((rez (make-array (list n 4) :initial-element nil))
+           (1st (range n))
+           (2nd (range n))
+           (i 0))
+       (dotimes (i n)
+         (let* ((r1 (random-elt 1st))
+                (r2 (random-elt 2nd))
+                (j1 (+ 2 (random 2)))
+                (j2 (+ 2 (random 2))))
+           (when (aref rez r1 j1)
+             (setf j1 (if (= j1 2) 3 2)))
+           (when (aref rez r2 j2)
+             (setf j2 (if (= j2 2) 3 2)))
+           (when (= r1 r2)
+             (setf j1 2
+                   j2 3))
+           (setf (aref rez i 0)   r1
+                 (aref rez i 1)   r2
+                 (aref rez r1 j1) i
+                 (aref rez r2 j2) i)
 
-        (setf 1st (remove r1 1st)
-              2nd (remove r2 2nd))))
+           (setf 1st (remove r1 1st)
+                 2nd (remove r2 2nd))))
+       (when (connected? rez)
+         (retrun rez)))))
 
-    rez))
+(defun connected? (2d-array)
+  (let* ((n (array-dimension 2d-array 0))
+         (gates (range n)))
+    (labels ((recur (next step)
+               (unless (= step 0)
+                 (print (setf gates (remove next gates)))
+                 (unless gates
+                   (return-from connected? t))
+                 (recur (aref 2d-array next 0) (1- step))
+                 (recur (aref 2d-array next 1) (1- step)))))
+      (recur 0 n))))
 
 (defun make-circuit (n)
   (let ((proto (make-node-array n)))
-    (
+    (dolist (i n)
+      (
